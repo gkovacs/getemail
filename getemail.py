3ec86ce9001e02d0822b44b871118589526b73f0
Geza Kovacs <gkovacs@stanford.edu>
2019-05-16 17:04:27 -0700

    improvements to utf-8 support

--- getemail.py
+++ getemail.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# md5: 240552f6ad16386caded4f93fbba18da
+# md5: 8018bae7565064fd8292d7a786e51bbd
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -16,6 +16,28 @@ connection.select()
 
 
 
+
+
+
+
+import arrow
+import email.utils
+import time
+import datetime
+
+def email_date_to_local_arrow(email_date):
+  date_tuple = email.utils.parsedate_tz(email_date)
+  return arrow.get(email.utils.mktime_tz(date_tuple)).to('America/Los_Angeles')
+
+def decode_header(header):
+  decoded,charset = email.header.decode_header(str(header))[0]
+  if charset == None:
+    return header
+    return decoded.decode('utf-8')
+  return decoded.decode(charset)
+
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -28,24 +50,24 @@ def get_emails_cs377u():
   messages = []
   result, data = connection.uid('search', None, "ALL")
   # search and return uids instead
-  i = len(data[0].split()) # data[0] is a space separate string
-  for x in range(i):
-    latest_email_uid = data[0].split()[x] # unique ids wrt label selected
+  uid_list = data[0].split()
+  for x,latest_email_uid in enumerate(uid_list):
     result, email_data = connection.uid('fetch', latest_email_uid, '(RFC822)')
     # fetch the email body (RFC822) for the given ID
     raw_email = email_data[0][1]
     email_message = email.message_from_bytes(raw_email)
     message = []
-    message.append('From: ' + email_message['From'])
+    message.append('From: ' + decode_header(email_message['From']))
     message.append('Subject: ' + email_message['Subject'])
-    message.append('Date: ' + email_message['Date'])
+    message.append('Date: ' + str(email_date_to_local_arrow(email_message['Date'])))
     #raw_email_string = raw_email.decode('utf-8')
     #print(raw_email_string)
     #email_message = email.message_from_string(raw_email_string)
     # this will loop through all the available multiparts in mail
     for part in email_message.walk():
      if part.get_content_type() == "text/plain": # ignore attachments/html
-      body = part.get_payload()
+      body = part.get_payload(decode=True).decode(part.get_content_charset())
+      #body = part.get_payload()
       message.append(body)
       #print(body.decode('utf-8'))
       #save_string = str("D:Dumpgmailemail_" + str(x) + ".eml")
@@ -57,7 +79,12 @@ def get_emails_cs377u():
      else:
       continue
     messages.append('<br>'.join(message))
-  return '<br><br>'.join(messages)
+  return '<br><br>'.join(reversed(messages))
+
+print(get_emails_cs377u())
+
+
+
 
 if __name__ == '__main__':
   app.run()
